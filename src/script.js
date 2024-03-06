a597371a051f87d8ba9999333a33985e188fcc55
swos <szymon.wos1319@gmail.com>
2024-03-06 19:18:46 +0100

    API calls for 3 day forecast and history

--- src/script.js
+++ src/script.js
@@ -12,10 +12,25 @@ const controller = (() => {
         console.log(weatherData);
     }
 
+    const getForecastInCity = async (city, amountOfDays = 3) => {
+        const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${city}&days=${amountOfDays}`);
+        const forecastData = await response.json();
+        console.log(forecastData);
+    }
+
+    const getHistoryInCity = async (city, date = new Date()) => {
+        date.setDate(date.getDate() - 1);
+        console.log(date);
+        const response = await fetch(`https://api.weatherapi.com/v1/history.json?key=${key}&q=${city}&dt=${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`);
+        const historyData = await response.json();
+        console.log(historyData);
+    }
+
     const start = () => {
         setKey();
         const city = prompt("What city you want check weather?", "London");
-        getWeatherInCity(city);
+        getForecastInCity(city);
+        getHistoryInCity(city);
     }
     return { start }
 })()
