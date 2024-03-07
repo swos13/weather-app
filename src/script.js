65b77f92190c77f3ae69aae4a098216c05de61c3
swos <szymon.wos1319@gmail.com>
2024-03-07 11:15:34 +0100

    Seperate API fetching into module

--- src/script.js
+++ src/script.js
@@ -1,36 +1,14 @@
+import weatherAPI from "./weather";
+
 const controller = (() => {
 
-    let key = "";
-
-    const setKey = () => {
-        key = prompt("What is your api key?");
-    }
-
-    const getWeatherInCity = async (city) => {
-        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=${city}`);
-        const weatherData = await response.json();
-        console.log(weatherData);
-    }
-
-    const getForecastInCity = async (city, amountOfDays = 3) => {
-        const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${city}&days=${amountOfDays}`);
-        const forecastData = await response.json();
-        console.log(forecastData);
-    }
-
-    const getHistoryInCity = async (city, date = new Date()) => {
-        date.setDate(date.getDate() - 1);
-        console.log(date);
-        const response = await fetch(`https://api.weatherapi.com/v1/history.json?key=${key}&q=${city}&dt=${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`);
-        const historyData = await response.json();
-        console.log(historyData);
-    }
 
     const start = () => {
-        setKey();
+        weatherAPI.setKey();
         const city = prompt("What city you want check weather?", "London");
-        getForecastInCity(city);
-        getHistoryInCity(city);
+        weatherAPI.getCurrent(city).then((response) => console.log(response));
+        weatherAPI.getForecast(city).then((response) => console.log(response));
+        weatherAPI.getHistory(city).then((response) => console.log(response));
     }
     return { start }
 })()
---
+++ src/weather.js
@@ -0,0 +1,32 @@
+const weatherAPI = (() => {
+
+    let key = "";
+
+    const setKey = () => {
+        key = prompt("What is your api key (from weatherapi.com)?");
+        if(key === "" || key == null)
+            setKey();
+    }
+
+    const getCurrent = async (city) => {
+        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${key}&q=${city}`);
+        const weatherData = await response.json();
+        return weatherData;
+    }
+
+    const getForecast = async (city, amountOfDays = 3) => {
+        const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${city}&days=${amountOfDays}`);
+        const forecastData = await response.json();
+        return forecastData;
+    }
+
+    const getHistory = async (city, date = new Date()) => {
+        date.setDate(date.getDate() - 1);
+        const response = await fetch(`https://api.weatherapi.com/v1/history.json?key=${key}&q=${city}&dt=${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`);
+        const historyData = await response.json();
+        return historyData;
+    }
+    return { setKey, getCurrent, getForecast, getHistory }
+})();
+
+export default weatherAPI;
